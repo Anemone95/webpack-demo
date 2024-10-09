28f0c66e64fc1e911b8d35cdd74bd29d2b1d4d6d
WenyuanXu <wenyuan.xu@cs.au.dk>
2024-10-09 15:16:11 +0200

    format bundle.js

--- demo2/dist/bundle.js
+++ demo2/dist/bundle.js
@@ -1,1 +1,27 @@
-(()=>{var o={903:(o,r)=>{r.g=function(){console.log("library A")}}},r={};function n(t){var e=r[t];if(void 0!==e)return e.exports;var i=r[t]={exports:{}};return o[t](i,i.exports,n),i.exports}(()=>{"use strict";function o(){console.log("library B, Function H")}let r=n(903).g;console.log("main"),console.log("library B, Function F"),r(),o(),o()})()})();
+(() => {
+    var o = {
+            903: (o, r) => {
+                r.g = function() {
+                    console.log("library A")
+                }
+            }
+        },
+        r = {};
+
+    function n(t) {
+        var e = r[t];
+        if (void 0 !== e) return e.exports;
+        var i = r[t] = {
+            exports: {}
+        };
+        return o[t](i, i.exports, n), i.exports
+    }(() => {
+        "use strict";
+
+        function o() {
+            console.log("library B, Function H")
+        }
+        let r = n(903).g;
+        console.log("main"), console.log("library B, Function F"), r(), o(), o()
+    })()
+})();
